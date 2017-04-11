6cb5f629b342ee90fdb24688d6636ac0ac3b544f
Darryl.Challenger <Darryl.Challenger@gmail.com>
2017-04-11 19:13:35 -0400

    Add project files.

---
+++ Equity1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Equity1.Startup))]
+namespace Equity1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
